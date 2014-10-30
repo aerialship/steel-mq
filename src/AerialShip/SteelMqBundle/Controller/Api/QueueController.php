6b778123e589557274c7f1ff57f60620ba50148f
Milos Tomic <milos.tomic@boutsourcing.com>
2014-10-30 22:42:35 +0100

    [API] Update Message Queue #16

--- src/AerialShip/SteelMqBundle/Controller/Api/QueueController.php
+++ src/AerialShip/SteelMqBundle/Controller/Api/QueueController.php
@@ -10,6 +10,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
@@ -70,4 +71,36 @@ class QueueController extends AbstractApiController
         return $this->handleView($this->view($queue));
     }
 
+    /**
+     * @Route("/{queueId}{slash}")
+     * @Method({"POST"})
+     * @ParamConverter("project", options={"id" = "projectId"})
+     * @ParamConverter("queue", options={"id" = "queueId"})
+     */
+    public function updateAction(Project $project, Queue $queue, Request $request)
+    {
+        if (false == $this->get('security.context')->isGranted(ProjectRole::PROJECT_ROLE_QUEUE, $project)) {
+            throw new AccessDeniedHttpException();
+        }
+        if ($queue->getProject()->getId() != $project->getId()) {
+            throw new BadRequestHttpException();
+        }
+
+        $form = $this->createForm('queue', $queue);
+
+        $form->submit($request, false);
+
+        if (false == $form->isValid()) {
+            return $this->handleView($this->view($form, 400));
+        }
+
+        $this->get('aerial_ship_steel_mq.defaulter.queue')->setDefaults($queue);
+
+        $em = $this->getEntityManager();
+
+        $em->persist($queue);
+        $em->flush();
+
+        return $this->handleView($this->view($queue));
+    }
 }
