3539d39d88b0011561a3970406fda2183a8963f2
Milos Tomic <milos.tomic@boutsourcing.com>
2014-10-31 15:07:29 +0100

    [API] Get info about a message queue #5

--- src/AerialShip/SteelMqBundle/Entity/Project.php
+++ src/AerialShip/SteelMqBundle/Entity/Project.php
@@ -12,7 +12,7 @@ use JMS\Serializer\Annotation as JMS;
  * @ORM\Table(name="smq_project")
  * @JMS\ExclusionPolicy("all")
  */
-class Project implements \JsonSerializable
+class Project
 {
     /**
      * @var int
@@ -197,21 +197,6 @@ class Project implements \JsonSerializable
         }
 
         return ProjectRole::toString($this->currentProjectRole->getRoles());
-    }
-
-    /**
-     * (PHP 5 &gt;= 5.4.0)<br/>
-     * Specify data which should be serialized to JSON
-     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
-     * @return mixed data which can be serialized by <b>json_encode</b>,
-     * which is a value of any type other than a resource.
-     */
-    public function jsonSerialize()
-    {
-        return array(
-            'id' => $this->getId(),
-            'title' => $this->getTitle(),
-        );
     }
 
 }
